529b583fe12203ed5c957118e9858243b51e83f1
KeithLord <ismaelcerrudo.12@gmail.com>
2022-09-10 11:04:55 -0300

    Dom

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,6 @@
 let tipo = prompt("Ingrese el tipo de electrodomestico");
 let precio = parseFloat(prompt("Ingrese el monto a pagar"));
-
-if(tipo === ""){
-    console.log("Ingrese un elemento valido");
-}
+let contenedor = document.getElementById("contenedor");
 
 
 class Product{
@@ -19,12 +16,14 @@ class Product{
     };
 }
 
-
 const productos = [];
 productos.push(new Product(tipo, precio));
+const dom = document.createElement('div');
 
 for (const producto of productos) {
     producto.Sumaiva();
     productos.forEach(producto => console.log(producto));
+    dom.innerHTML = `<h3>El tipo de producto es: ${producto.tipo} y el precio es: ${producto.precio}</h3>`
+    contenedor.append(dom);
 };
 
